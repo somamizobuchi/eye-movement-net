21b5da260ffde9b1b1cc71737389175e3cf025a1
Soma Mizobuchi <smizobuc@u.rochester.edu>
2025-04-09 10:34:29 -0400

    Package metadata

--- train_grid.sh
+++ train_grid.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-uv run train_grid.py --grid_search=True --grid_search_iterations=250000 --alpha_values "[0, 0.01, 0.1, 1]" --delta_values "[0, 0.01, 0.1, 1]" --gamma_values "[0, 0.01, 0.1, 1]" --beta_values "[0, 0.01, 0.1, 1]" --device="cuda"
+uv run train_grid.py --grid_search=True --grid_search_iterations=250000 --alpha_values "[0.01]" --delta_values "[0.01]" --gamma_values "[0.01]" --beta_values "[0.01]" --device="cuda"
