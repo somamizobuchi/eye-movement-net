882adb482b1ef34a2594f1a9a4eb8c5493cdb713
Soma Mizobuchi <smizobuc@u.rochester.edu>
2025-04-15 12:59:24 -0400

    setting dataset __len__ to smaller value results in faster training

--- datasets/video_dataset.py
+++ datasets/video_dataset.py
@@ -28,7 +28,7 @@ class VideoDataset(Dataset):
         self.length = self.data.shape[0]
 
     def __len__(self) -> int:
-        return 1_000_000
+        return self.length
 
     def __getitem__(self, index) -> Tuple[torch.Tensor]:
         """Randomly select a video index."""
