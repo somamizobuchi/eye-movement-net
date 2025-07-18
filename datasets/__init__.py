388868d7bd3d2cabcd6b81decbe314368de7b433
Soma Mizobuchi <smizobuc@u.rochester.edu>
2025-07-18 13:33:56 -0400

    merge conflict resolved

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,8 +4,5 @@ import numpy as np
 from typing import Tuple
 
 from .video_dataset import VideoDataset
-<<<<<<< HEAD
 from .recon_dataset import ReconDataset
-=======
 from .filtered_video_dataset import FilteredVideoDataset
->>>>>>> 61a5a379e415864eaf50d08b6f98fa0b31fc6661
